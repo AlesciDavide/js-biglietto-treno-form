9e51b3c1721b92b47ac2347819a41b3b4879c9ca
AlesciDavide <davi.alesci@email.com>
2024-05-09 16:24:23 +0200

    import bootstrap- add form style- add card

--- js/script.js
+++ js/script.js
@@ -12,16 +12,28 @@ bottone.addEventListener('click', function(){
 
     if (isNaN(numberKmUser.value) || isNaN(numberAgeUser.value) || numberKmUser.value <= 0 || numberAgeUser.value <= 0 || numberAgeUser.value > 150) {
         alert("Attento hai inserito valori non validi");
-        
+
     }else if(numberAgeUser.value >= 63){
         priceTicket = priceTicket - (priceTicket * 0.37893);
         console.log("il prezzo del tuo biglietto essendo over 63 è di:" + " " + (priceTicket.toFixed(2)));
 
+        let prezzoBigliettoHone = document.querySelector('div#generateCard');
+
+        prezzoBigliettoHone.innerHTML += ('<div class="card my_grandezza_card mb-3">' + '<img src="./img/1.jpg" class="card-img-top img-thumbnail" alt="treno">' + '<div class="card-body">' + '<p class="card-text">Il prezzo del tuo biglietto</p>' + '</div>' + '<ul class="list-group list-group-flush">' + '<li class="list-group-item">' + 'Prezzo: ' + priceTicket.toFixed(2) + '€' + '</li>' + '<li class="list-group-item">' + 'La tua età: ' + numberAgeUser.value + ' anni' + '</li>' + '<li class="list-group-item">' + 'I chilometri che devi percorrere: ' + numberKmUser.value + 'Km' + '</li>' + '</ul>' + '</div>');
+
     }else if( numberAgeUser.value <= 21){
         priceTicket = priceTicket - (priceTicket * 0.24552);
         console.log("il prezzo del tuo biglietto essendo under 21 è di:" + " " + (priceTicket.toFixed(2)));
+
+        let prezzoBigliettoHone = document.querySelector('div#generateCard');
+
+        prezzoBigliettoHone.innerHTML += ('<div class="card my_grandezza_card mb-3">' + '<img src="./img/1.jpg" class="card-img-top img-thumbnail" alt="treno">' + '<div class="card-body">' + '<p class="card-text">Il prezzo del tuo biglietto</p>' + '</div>' + '<ul class="list-group list-group-flush">' + '<li class="list-group-item">' + 'Prezzo: ' + priceTicket.toFixed(2) + '€' + '</li>' + '<li class="list-group-item">' + 'La tua età: ' + numberAgeUser.value + ' anni' + '</li>' + '<li class="list-group-item">' + 'I chilometri che devi percorrere: ' + numberKmUser.value + 'Km' + '</li>' + '</ul>' + '</div>');
     }else{
         console.log("Il prezzo del tuo biglietto non rientrando nell'età per un possibile sconto è:" + " " + (priceTicket.toFixed(2)));
+
+        let prezzoBigliettoHone = document.querySelector('div#generateCard');
+
+        prezzoBigliettoHone.innerHTML += ('<div class="card my_grandezza_card mb-3">' + '<img src="./img/1.jpg" class="card-img-top img-thumbnail" alt="treno">' + '<div class="card-body">' + '<p class="card-text">Il prezzo del tuo biglietto</p>' + '</div>' + '<ul class="list-group list-group-flush">' + '<li class="list-group-item">' + 'Prezzo: ' + priceTicket.toFixed(2) + '€' + '</li>' + '<li class="list-group-item">' + 'La tua età: ' + numberAgeUser.value + ' anni' + '</li>' + '<li class="list-group-item">' + 'I chilometri che devi percorrere: ' + numberKmUser.value + 'Km' + '</li>' + '</ul>' + '</div>');
     }
 
     
