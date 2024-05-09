778394b6eb636cd2d44dfeb38c47104f285fc179
AlesciDavide <davi.alesci@email.com>
2024-05-09 13:29:30 +0200

    dichiarazione variabili

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let numberAgeUser = document.querySelector('input');
+
+let numberKmUser = document.querySelector('input#userKm');
+
+let prezzoBiglietto = (numberKmUser * 0.267113);
+
+let bottone = document.querySelector('button#sendData')
